ee00043ee6b43aad70c07c68f78a54b10efc77ac
Geane Ferreira <geanepereira@seduc.ro.gov.br>
2020-11-09 11:41:08 -0400

    Acrescentando models e controllers

---
+++ Controllers/FichaTecnicaController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nutri.Controllers
+{
+    public class FichaTecnicaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+
+
+    }
+}
---
+++ Models/FichaTecnica.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+namespace Nutri.Models
+{
+    public class FichaTecnica
+    {
+        public Guid Id { get; set; }
+
+        public string Ingredientes { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string Image { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public string ModoPreparo { get; set; }
+
+        public int Rendimento { get; set; }
+
+        public int CustoTotal { get; set; }
+
+        public int CustoPorcao { get; set; }
+
+        public DateTimeOffset? DataConclusão { get; set; }
+
+
+    }
+
+
+}
