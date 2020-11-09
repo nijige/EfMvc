c2f5d7264a13fb5a578c0784c41ca024e3b68a65
Geane Ferreira <geanepereira@seduc.ro.gov.br>
2020-11-08 20:03:29 -0400

    Atualizando o projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nutri.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
