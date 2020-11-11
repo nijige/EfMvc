8e471f4cb0fe2ad15e91f105b22778459f86ee57
Geane Ferreira <geanepereira@seduc.ro.gov.br>
2020-11-11 17:42:41 -0400

    atualizando banco

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
 namespace Nutri.Data
 {
     public class DataDbContext : DbContext
